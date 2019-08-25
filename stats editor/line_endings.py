6413b08dbe0dea0bdf00a316f1d605a5915076a3
Jason Breijak <jbmreija@gmail.com>
2019-08-24 21:58:34 -0400

    set max turret weight at 250000 for rotation speeds otherwise overflow

--- stats editor/line_endings.py
+++ stats editor/line_endings.py
@@ -1,12 +1,10 @@
-
-
+# Formatting from Warzone stats editor is not readable. This script makes the .json files readable
 import json
 import os
 
-
-for i in os.listdir(".\\stats"):
-    with open(f".\\stats\\{i}") as f:
+for each_file in os.listdir(".\\stats"):
+    with open(f".\\stats\\{each_file}") as f:
         JSON_DATA = json.load(f)
 
-    with open(f".\\stats\\{i}", "w") as f:
+    with open(f".\\stats\\{each_file}", "w") as f:
         f.write(json.dumps(JSON_DATA, indent=4, sort_keys=True))
